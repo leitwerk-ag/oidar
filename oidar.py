315e5ca23827373710ab8223f9b65b34af7e1fdf
Justin Otherguy <justin@justinotherguy.org>
2022-02-12 23:24:23 +0100

    add auto find of audio files

--- oidar.py
+++ oidar.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
-import os, time, sys
+import os, time, sys, fnmatch
 from pygame import mixer  # Load the popular external library
 
-critical_hosts=['9.9.9.9', '8.8.8.8', '1.1.1.1']
+critical_hosts=['9.9.9.9', '8.8.8.8', '1.1.1.1', '192.168.0.1']
 warning_hosts=['1.2.3.4']
 
 
+audio_dir='./audio'
 fail_counter=0
-tolerance=1
+tolerance=0
+
+def find(pattern, path):
+  result = []
+  for root, dirs, files in os.walk(path):
+    for name in files:
+      if fnmatch.fnmatch(name, pattern):
+        result.append(os.path.join(root, name))
+  return result
+
+song_list=find('*.mp3', audio_dir)
+if len(song_list) < 1:
+  # no songs found
+  print('Error: no songs found in directory ' + audio_dir)
+  sys.exit(1)
 
 for host in critical_hosts:
   print(host)
@@ -28,8 +43,10 @@ elif fail_counter <= tolerance:
 else:
   # omg - the internet is down!1!!!
   print("Jen, the internet is down! Let's play some music!")
-  mixer.init()
-  mixer.music.load('./audio/All_My_Friends_Hate_Me_-_Blood.mp3')
-  mixer.music.play()
-  while mixer.music.get_busy():  # wait for music to finish playing
-    time.sleep(1)
+  #mixer.init()
+  for song in song_list:
+    print(song)
+    #mixer.music.load(song)
+    #mixer.music.play()
+    #while mixer.music.get_busy():  # wait for music to finish playing
+    #  time.sleep(1)
